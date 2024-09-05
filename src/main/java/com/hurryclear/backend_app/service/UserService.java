27efa1c9d398f0e056341a3b766ad5845adb3860
hurryclear <hurjiangde@gmail.com>
2024-09-05 16:20:37 +0200

    change author name

--- src/main/java/com/hurryclear/backend_app/service/UserService.java
+++ src/main/java/com/hurryclear/backend_app/service/UserService.java
@@ -4,7 +4,7 @@ import com.hurryclear.backend_app.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
-* @author hurjiang
+* @author hurryclear
 * @description 针对表【user】的数据库操作Service
 * @createDate 2024-08-30 18:23:02
 */

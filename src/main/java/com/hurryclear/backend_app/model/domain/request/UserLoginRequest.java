12beebcb591d071481ddb878b87d5a7e9cdd60f2
hurryclear <hurjiangde@gmail.com>
2024-09-06 19:17:56 +0200

    add UserLoginRequest

---
+++ src/main/java/com/hurryclear/backend_app/model/domain/request/UserLoginRequest.java
@@ -0,0 +1,20 @@
+package com.hurryclear.backend_app.model.domain.request;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+
+/**
+ * request body for user register
+ */
+@Data
+public class UserLoginRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 6018562549529666914L;
+
+    private String userAccount;
+    private String userPassword;
+}

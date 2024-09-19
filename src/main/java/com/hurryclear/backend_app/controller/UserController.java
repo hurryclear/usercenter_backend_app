2298c915e17356b337f7d244de8ee7cbe0b1b5b3
hurryclear <hurjiangde@gmail.com>
2024-09-19 22:20:57 +0200

    add api in controller for search and delete

--- src/main/java/com/hurryclear/backend_app/controller/UserController.java
+++ src/main/java/com/hurryclear/backend_app/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.hurryclear.backend_app.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hurryclear.backend_app.model.domain.User;
 import com.hurryclear.backend_app.model.domain.request.UserRegisterRequest;
 import com.hurryclear.backend_app.service.UserService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.hurryclear.backend_app.constant.UserConstant.ADMIN_ROLE;
+import static com.hurryclear.backend_app.constant.UserConstant.USER_LOGIN_STATE;
 
 
 @RestController
@@ -57,4 +62,50 @@ public class UserController {
         return userService.userLogin(userAccount, userPassword, request);
 
     }
+
+    @GetMapping("/search")
+    public List<User> searchUsers(String username, HttpServletRequest request){
+
+        if (!isAdmin(request)) {
+            return new ArrayList<>();
+        }
+
+        // what is QueryWrapper?
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if (StringUtils.isNotBlank(username)) {
+            queryWrapper.like("username", username);
+        }
+
+        List<User> userList = userService.list(queryWrapper);
+        return userList.stream().map(user -> userService.getSafetyUser(user)).collect(Collectors.toList());
+    }
+
+    @PostMapping("/delete")
+    public boolean deleteUser(@RequestBody long id, HttpServletRequest request) {
+
+        if (!isAdmin(request)) {
+            return false;
+        }
+
+        if (id <= 0) {
+            return false;
+        }
+        return userService.removeById(id); // logic delete
+    }
+
+    /**
+     * is admin?
+     * @param request
+     * @return boolean
+     */
+    private boolean isAdmin (HttpServletRequest request) {
+        // this interface is only for admin accessible, we have to determine the role
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User user = (User) userObj;
+        if (user == null || user.getUserRole() != ADMIN_ROLE) {
+            return false;
+        }
+        return true;
+    }
+
 }

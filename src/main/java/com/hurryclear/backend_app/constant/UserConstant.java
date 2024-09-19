fac7f75c948af77caf499e571fdeb1d61ae22183
hurryclear <hurjiangde@gmail.com>
2024-09-19 22:20:05 +0200

    add some constants for user

---
+++ src/main/java/com/hurryclear/backend_app/constant/UserConstant.java
@@ -0,0 +1,24 @@
+package com.hurryclear.backend_app.constant;
+
+/**
+ * user constant
+ * @author hurryclear
+ */
+public interface UserConstant {
+
+    /**
+     * key of user login's status
+     */
+    String USER_LOGIN_STATE = "userLoginState";
+
+
+    /**
+     * standard role/authority
+     */
+    int DEFAULT_ROLE = 0;
+
+    /**
+     * admin role
+     */
+    int ADMIN_ROLE = 1;
+}
